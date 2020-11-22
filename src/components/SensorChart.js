efdbcdfda3b8ec20d7b09b80cdd5785cb910da09
HotSpor <hotspor00@gmail.com>
2020-11-22 21:36:22 +0200

    Fixed chart render problem

--- src/components/SensorChart.js
+++ src/components/SensorChart.js
@@ -1,80 +1,64 @@
-import React, {Component} from 'react';
-import {Line} from 'react-chartjs-2';
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
 
 
-const options = {
-    title: {
-        display: true,
-        text: "Chart Title"
-    },
-    scales: {
-        yAxes: [
-            {
-                ticks: {
-                    suggestedMin: 20,
-                    suggestedMax: 50
+
+
+function SensorChart(props) {
+
+    console.log(props, "props")
+
+    const options = {
+        title: {
+            display: true,
+            text: "Chart Title"
+        },
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        suggestedMin: 20,
+                        suggestedMax: 50
+                    }
                 }
+            ]
+        }
+    };
+
+    const legend = {
+
+        display: true,
+        position: "bottom",
+        reverse: true,
+        labels: {
+            fontColor: "#323130",
+            fontSize: 14
+        }
+    };
+    
+    const data = {
+        labels: props.labels,
+        datasets: [
+            {
+                label: "Humidity",
+                borderColor: "rgb(9,0,192)",
+                data: props.humidity
+            },
+            {
+                label: "Temperature",
+                borderColor: "red",
+                data: props.temperature
             }
         ]
     }
-};
-
-const legend = {
-
-    display: true,
-    position: "bottom",
-    reverse: true,
-    labels: {
-        fontColor: "#323130",
-        fontSize: 14
-    }
-};
-
-class SensorChart extends Component {
 
 
-    constructor(props) {
-        super(props);
-        this.chartReference = React.createRef()
-        this.state = {
-            data: {
-                labels: props.labels,
-                datasets: [
-                    {
-                        label: "Humidity",
-                        borderColor: "rgb(9,0,192)",
-                        data: props.humidity
-                    },
-                    {
-                        label: "Temperature",
-                        borderColor: "red",
-                        data: props.temperature
-                    }
-                ]
-            }
-        }
-    }
 
-
-    componentDidMount() {
-        console.log(this.chartReference); // returns a Chart.js instance reference
-        console.log(this.props.labels)
-        console.log(this.props.temperature)
-        console.log(this.props.humidity)
-        console.log(this.state.data)
-        console.log(this.state.data.datasets[0].data)
-        console.log(this.state.data.datasets[1].data)
-    }
-
-
-    render() {
-        return (
-            <div>
-                <h5>Line Example</h5>
-                <Line ref={this.chartReference} data={this.state.data} legend={legend} options={options}/>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Line data={data} legend={legend} options={options} />
+        </div>
+    );
 
 }
 
--- src/components/SensorData.js
+++ src/components/SensorData.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Icon from '@mdi/react'
-import {mdiThermometer, mdiWaterPercent, mdiCalendarRange} from '@mdi/js';
+import { mdiThermometer, mdiWaterPercent, mdiCalendarRange } from '@mdi/js';
 import './SensorData.css';
 import SensorChart from "./SensorChart";
 
@@ -10,49 +10,60 @@ function SensorData() {
     const [humi, setHumi] = React.useState("");
     const [temp, setTemp] = React.useState("");
     const [date, setDate] = React.useState("");
-
-    const [labels] = React.useState([]);
-    const [humidity] = React.useState([]);
-    const [temperature] = React.useState([]);
+    const [done, isDone] = React.useState(false);
 
 
-    function fetchData() {
-        fetch('https://asema.herokuapp.com/datalist/100')
+    const [labels, setLabels] = React.useState([]);
+    const [humidity, setHumidity] = React.useState([]);
+    const [temperature, setTemperature] = React.useState([]);
+
+    let labelsTemp = [];
+    let humidityTemp = [];
+    let temperatuerTemp = [];
+
+    const fetchData = async () => {
+        await fetch('https://asema.herokuapp.com/datalist/100')
             .then(response => response.json())
             .then(responseData => {
                 setDate(responseData[0].stringTime);
                 setHumi(responseData[0].humidity);
                 setTemp(responseData[0].temperature);
 
+
                 for (let i = 0; i < 100; i++) {
-                    labels.push(responseData[i].stringTime)
-                    humidity.push(responseData[i].humidity)
-                    temperature.push(responseData[i].temperature)
+                    labelsTemp.push(responseData[i].stringTime)
+                    humidityTemp.push(responseData[i].humidity)
+                    temperatuerTemp.push(responseData[i].temperature)
                 }
-
-
-
+                setLabels(labelsTemp)
+                setHumidity(humidityTemp)
+                setTemperature(temperatuerTemp)
+                isDone(true)
             })
     }
 
     React.useEffect(() => {
         fetchData();
-    }, []); //Compiler gives warning on this?   Line 40:8:  React Hook React.useEffect has a missing dependency: 'fetchData'.
-                    //if removed chart loops few times and data just looks strange :O
+    }, []);
 
 
-    return (
-        <div className="App">
-            <h1>Sensor data</h1>
-
-            <h3><Icon path={mdiWaterPercent}/> Humidity: {humi}%</h3>
-            <h3><Icon path={mdiThermometer}/>Temperature: {temp}°C</h3>
-            <h3><Icon path={mdiCalendarRange}/>Time: {date}</h3>
-            <div className="Chart">
-                <SensorChart labels={labels} humidity={humidity} temperature={temperature}/>
+    if(!isDone){
+        return <h1>Loading....</h1>
+    }else {
+        return (
+            <div className="App">
+                <h1>Sensor data</h1>
+    
+                <h3><Icon path={mdiWaterPercent} /> Humidity: {humi}%</h3>
+                <h3><Icon path={mdiThermometer} />Temperature: {temp}°C</h3>
+                <h3><Icon path={mdiCalendarRange} />Time: {date}</h3>
+                <div className="Chart">
+                    <SensorChart labels={labels} humidity={humidity} temperature={temperature} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default SensorData;
